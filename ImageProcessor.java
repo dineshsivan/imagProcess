3c671543d513c23b7b001c0383a02998240cfd03
dineshsivan <dineshsivan169@gmail.com>
2023-02-26 14:32:44 +0800

    Update ImageProcessor.java
    Introduce MarvinImage

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -1,42 +1,57 @@
 package com.dinesh.intrvw;
 
-import javax.imageio.ImageIO;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
 import java.awt.image.ImageProducer;
 import java.awt.image.RGBImageFilter;
-import java.io.*;
-import java.awt.*;
-import java.net.URL;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+import marvinplugins.MarvinPluginCollection;
+import marvinplugins.MarvinPluginCollection.*;
+
 
 public class ImageProcessor {
 	public static void main(String[] args) throws IOException {
 		System.setProperty("http.agent", "Chrome");
 		int imgCount = 2;
 
-		URL url = new URL("https://scontent.fsin10-1.fna.fbcdn.net/v/t31.18172-8/14633226_1232078330168467_1704321397909539930_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9267fe&_nc_ohc=lF3S--8YeqgAX9EbH2d&_nc_ht=scontent.fsin10-1.fna&oh=00_AfDktn0bKl7br_vFr4PEK61tkr0qv96pLRcOLl1ODf_CBQ&oe=63E77796");
+//		URL url = new URL("E:\\Test Img\\Source.jpg");
+		File file = new File("E:\\Test Img\\Source.jpg");
 
-		InputStream is = url.openStream();
-		BufferedImage image = ImageIO.read(is);
+		// InputStream is = url.openStream();
+		BufferedImage image = ImageIO.read(file);
 
 		// initializing array to hold subimages
 		BufferedImage imgs[] = new BufferedImage[imgCount];
 
 		// Equally dividing original image into subimages
 
-		int First_img_Width = (int) Math.round(image.getWidth() * 0.6);
+		int First_img_Width = (int) Math.round(image.getWidth() * 0.4);
 		int Second_img_Width = image.getWidth() - First_img_Width;
 		int subimage_Height = image.getHeight();
 
+		
+		
 		createImage(image, imgs, First_img_Width, subimage_Height, 0, 0, 0, First_img_Width, subimage_Height);
 		createImage(image, imgs, Second_img_Width, subimage_Height, 1, First_img_Width, 0,
 				(First_img_Width + Second_img_Width), subimage_Height);
 
 		printImg(imgs, "jpg", imgCount);
-		
 
 		printImg(imgs, "png", imgCount);
+		
+	
 		
 		System.out.println("Sub-images have been created.");
 	}
@@ -47,10 +62,14 @@ public class ImageProcessor {
 			filename.append("E:\\Test Img\\img").append(i).append(".").append(type);
 			File outputFile = new File(filename.toString());
 			if ("png".equalsIgnoreCase(type)) {
-				imgs[i] = changeImageCoulor(imageToBufferedImage(makeColorTransparent(imgs[i], Color.white)));
-			} 
+			//	imgs[i] =  imageToBufferedImage(makeColorTransparent(imgs[i],Color.BLUE));
+			//	imgs[i] =  changeImageCoulor(imgs[i] );
+				printTRansparent(imgs[i], filename.toString());
+			}else {
+				ImageIO.write(convertToGrayScale(imgs[i]), type, outputFile);
+			}
+
 			
-			ImageIO.write(imgs[i], type, outputFile);
 		}
 	}
 
@@ -76,21 +95,21 @@ public class ImageProcessor {
 
 		ImageFilter filter = new RGBImageFilter() {
 
-				public int markerRGB = color.getRGB() | 0xFFFFFFFF;
+			public int markerRGB = color.getRGB() | 0xFFFFFFFF;
 
-				public final int filterRGB(final int x, final int y, final int rgb) {
-					if ((rgb | 0xFF000000) == markerRGB) {
-						return 0x00FFFFFF & rgb;
-					} else {
-						return rgb;
-					}
+			public final int filterRGB(final int x, final int y, final int rgb) {
+				if ((rgb | 0xFF000000) == markerRGB) {
+					return 0x00FFFFFF & rgb;
+				} else {
+					return rgb;
 				}
-			};
-		
+			}
+		};
 
 		final ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
 		return Toolkit.getDefaultToolkit().createImage(ip);
 	}
+
 	public static BufferedImage makeImageTranslucent(BufferedImage source, double alpha) {
 		BufferedImage target = new BufferedImage(source.getWidth(), source.getHeight(),
 				java.awt.Transparency.TRANSLUCENT);
@@ -105,31 +124,62 @@ public class ImageProcessor {
 		// Return the image
 		return target;
 	}
-	
+
 	public static BufferedImage changeImageCoulor(BufferedImage img) {
 		int width = img.getWidth();
-        int height = img.getHeight();
-  
-        // convert to red image
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int p = img.getRGB(x, y);
-  
-                int a = (p >> 24) & 0xff;
-                int r = (p >> 16) & 0xff;
-  
-                // set new RGB keeping the r
-                // value same as in original image
-                // and setting g and b as 0.
-                p = (a << 24) | (0 << 16) | (0 << 8) | 0;
-  
-                img.setRGB(x, y, p);
-            }
-        }
-        
-        
-        
-        return img;
+		int height = img.getHeight();
+
+		// convert to red image
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int p = img.getRGB(x, y);
+
+				int a = (p >> 24) & 0xff;
+				int r = (p >> 16) & 0xff;
+				  int g = (p >> 8) & 0xff;
+			        // get blue
+			        int b = p & 0xff;
+				// set new RGB keeping the r
+				// value same as in original image
+				// and setting g and b as 0.
+				 p = (a << 24) | (r << 16) | (g << 8) | b;
+			        img.setRGB(x, y, p);
+			}
+		}
+
+		return img;
 	}
+
+	public static BufferedImage convertToGrayScale(BufferedImage image) {
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int p = image.getRGB(x, y);
+				int a = (p >> 24) & 0xff;
+				int r = (p >> 16) & 0xff;
+				int g = (p >> 8) & 0xff;
+				int b = p & 0xff;
+				int avg = (r + g + b) / 3;
+				p = (avg << 24) | (avg << 16) | (avg << 8) | avg;
+				image.setRGB(x, y, p);
+			}
+		}
+		return image;
+	}
+	
+	public static void printTRansparent(BufferedImage img,String outputString) {
+		
+		//MarvinImage mImg = MarvinImageIO.loadImage("E:\\Test Img\\Source.png");
+		MarvinImage mImg = new MarvinImage(img,"png");
+		MarvinPluginCollection.blackAndWhite(mImg, 150);
+		MarvinPluginCollection.boundaryFill(mImg.clone(), mImg, 0, 0, Color.white, 150);
+		mImg.setColorToAlpha(0, 0xFFFFFFFF);
+		MarvinPluginCollection.alphaBoundary(mImg, 5);
+		MarvinImageIO.saveImage(mImg, outputString);
+	}
+
+	
 	
 }
